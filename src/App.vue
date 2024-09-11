0f0d26eb49944a4b82ba457d65494af0fd7681fe
fisheeesh <swanphyo444@gmail.com>
2024-09-11 17:59:49 +0700

    add pj form style and nav bar style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
+  <NavBar></NavBar>
   <router-view/>
 </template>
-
+<script>
+import NavBar from './components/NavBar'
+export default {
+  components: { NavBar },
+  
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AddProject from '@/views/AddProject.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path : '/addProject',
+    name : 'addProject',
+    component : AddProject
   }
 ]
 
